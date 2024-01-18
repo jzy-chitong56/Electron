b31151b753745e0a8b297b8b77885da02e7571fc
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-18 16:36:33 +0800

    Update doinstall.js

--- src/assets/files/doinstall.js
+++ src/assets/files/doinstall.js
@@ -2,6 +2,6 @@ const electronService = require('app/core/services/electron.service');
 
 function executeCommand(install) {  
   // 在这里执行你的指令或操作  
-  console.log("执行指令: " + command);  
+ipcRenderer.send('install-folder-noc-ROC');
 
 }
