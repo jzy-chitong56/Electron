14f2c86b0330d1f070cda7c5f06001863527409b
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-18 16:47:45 +0800

    Update doinstall.js

--- src/assets/files/doinstall.js
+++ src/assets/files/doinstall.js
@@ -1,14 +1,10 @@
 const electronService = require('app/core/services/electron.service');
 
-function executeCommand(install) {  
-  // 在这里执行你的指令或操作  
 ipcRenderer.send('install-folder-noc-ROC');
-
 
   function executeFunction(param1, param2) {  
   // 在这里实现你的逻辑  
-  if (typeof param1 === 'number' && typeof param2 === 'number') {  
-    // 判断参数是否为数值类型  
+
     if (param1 > 0 && param2 > 0) {  
       // 执行正数情况下的指令  
       console.log('两个参数都是正数');  
@@ -23,6 +19,6 @@ ipcRenderer.send('install-folder-noc-ROC');
     // 参数类型不匹配时的处理逻辑  
     console.log('参数类型不匹配');  
   }  
-}
+
 
 }
