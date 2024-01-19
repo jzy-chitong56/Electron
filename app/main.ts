bdeb470cd0f159cf0d50ba6cf089b2dd0f22543c
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-19 21:39:29 +0800

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -76,12 +76,12 @@ const execInstall = async (signal, commander: boolean = true, isMap: boolean = f
   const controller = new AbortController();
   const response = dialog.showOpenDialogSync(win, {
     // TODO: add i18n here
-    title : isMap ? "Open WC3 Map File": "Open AMAI Maps Directory",
+    title : isMap ? "打开地图": "打开地图目录",
     // TODO: Change to let multiples selections when is map
-    properties: isMap ? ['openFile'] : ['openDirectory'],
+    properties: isMap ? ['打开地图'] : ['打开地图目录'],
     // TODO: add i18n here
     filters: isMap ? [
-      { name: 'WC3 Map File', extensions: ['w3x', 'w3m'] },
+      { name: '地图文件', extensions: ['w3x', 'w3m'] },
     ] : null,
   });
 
