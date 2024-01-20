f5baa1285c818e88a534e8314970fee87b57a365
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-21 02:31:11 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import { ipcRenderer } from 'electron';
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const Button_ROC = document.getElementById('imageContainer.ROC');
+    const Button_TFT = document.getElementById('imageContainer.TFT');
+    const Button_REF = document.getElementById('imageContainer.REF');
+    if (Button_ROC) {
+        button.addEventListener('click', () => {
+            if (SetMode_Switch_State && BJ_Switch_State) {
+                ipcRenderer.send('install-folder-ROC');
+            } else if (SetMode_Switch_State && !BJ_Switch_State) {
+                ipcRenderer.send('install-folder-noc-ROC');
+            } else if (!SetMode_Switch_State && BJ_Switch_State) {
+                ipcRenderer.send('install-map-ROC');
+            } else if (!SetMode_Switch_State && !BJ_Switch_State) {
+                ipcRenderer.send('install-map-noc-ROC');
+            }
+        }
+    )}
+    if (Button_TFT) {
+        button.addEventListener('click', () => {
+            if (SetMode_Switch_State && BJ_Switch_State) {
+                ipcRenderer.send('install-folder-TFT');
+            } else if (SetMode_Switch_State && !BJ_Switch_State) {
+                ipcRenderer.send('install-folder-noc-TFT');
+            } else if (!SetMode_Switch_State && BJ_Switch_State) {
+                ipcRenderer.send('install-map-TFT');
+            } else if (!SetMode_Switch_State && !BJ_Switch_State) {
+                ipcRenderer.send('install-map-noc-TFT');
+            }
+        }
+    )}
+    if (Button_REF) {
+        button.addEventListener('click', () => {
+            if (SetMode_Switch_State && BJ_Switch_State) {
+                ipcRenderer.send('install-folder');
+            } else if (SetMode_Switch_State && !BJ_Switch_State) {
+                ipcRenderer.send('install-folder-noc');
+            } else if (!SetMode_Switch_State && BJ_Switch_State) {
+                ipcRenderer.send('install-map');
+            } else if (!SetMode_Switch_State && !BJ_Switch_State) {
+                ipcRenderer.send('install-map-noc');
+            }
+        }
+        )}
+});
