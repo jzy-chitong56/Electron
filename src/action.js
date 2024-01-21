17c584070557a96a0768d43351e03c03b58e3863
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-21 14:31:29 +0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,39 +1,39 @@
-const { ipcRenderer } = ('electron')
+const { ElectronService } = ('../electron/electron.service')
 
 
 image_Container_ROC.addEventListener('click', () => {
     if (Mode_Switch_State && BJ_Switch_State) {
-        ipcRenderer.send('install-folder-ROC')
+        this.electronService.ipcRenderer.send('install-folder-ROC')
     } else if (Mode_Switch_State && !BJ_Switch_State) {
-        ipcRenderer.send('install-folder-noc-ROC')
+        this.electronService.ipcRenderer.send('install-folder-noc-ROC')
     } else if (!Mode_Switch_State && BJ_Switch_State) {
-        ipcRenderer.send('install-map-ROC')
+        this.electronService.ipcRenderer.send('install-map-ROC')
     } else if (!Mode_Switch_State && !BJ_Switch_State) {
-        ipcRenderer.send('install-map-noc-ROC')
+        this.electronService.ipcRenderer.send('install-map-noc-ROC')
     }
 })
 
 
 image_Container_TFT.addEventListener('click', () => {
     if (Mode_Switch_State && BJ_Switch_State) {
-        ipcRenderer.send('install-folder-TFT')
+        this.electronService.ipcRenderer.send('install-folder-TFT')
     } else if (Mode_Switch_State && !BJ_Switch_State) {
-        ipcRenderer.send('install-folder-noc-TFT')
+        this.electronService.ipcRenderer.send('install-folder-noc-TFT')
     } else if (!Mode_Switch_State && BJ_Switch_State) {
-        ipcRenderer.send('install-map-TFT')
+        this.electronService.ipcRenderer.send('install-map-TFT')
     } else if (!Mode_Switch_State && !BJ_Switch_State) {
-        ipcRenderer.send('install-map-noc-TFT')
+        this.electronService.ipcRenderer.send('install-map-noc-TFT')
     }
 })
 
 image_Container_REF.addEventListener('click', () => {
     if (Mode_Switch_State && BJ_Switch_State) {
-        ipcRenderer.send('install-folder')
+        this.electronService.ipcRenderer.send('install-folder')
     } else if (Mode_Switch_State && !BJ_Switch_State) {
-        ipcRenderer.send('install-folder-noc')
+        this.electronService.ipcRenderer.send('install-folder-noc')
     } else if (!Mode_Switch_State && BJ_Switch_State) {
-        ipcRenderer.send('install-map')
+        this.electronService.ipcRenderer.send('install-map')
     } else if (!Mode_Switch_State && !BJ_Switch_State) {
-        ipcRenderer.send('install-map-noc')
+        this.electronService.ipcRenderer.send('install-map-noc')
     }
 })
