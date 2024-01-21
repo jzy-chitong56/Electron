7b1965f3b171b587fc7aa712538e68a21008d314
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-21 19:22:37 +0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,28 +1,44 @@
-const { ElectronService } = ('src/app/core/services/electron/electron.service')
+var { Injectable } = require('@angular/core');
+const { ElectronService } = ('src/app/core/services/electron/electron.service');
 
+var ElectronService = (function() {
+  function ElectronService() {
+  }
+  return ElectronService;
+})();
+
+ElectronService.ɵprov = { 
+  token: ElectronService, 
+  providedIn: 'root',
+};
+class ElectronService {  
+  constructor(electronService) {  
+    this.electronService = ElectronService;  
+  }  
+}
 
 image_Container_ROC.addEventListener('click', () => {
     if (Mode_Switch_State && BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-folder-ROC')
+        ElectronService.ipcRenderer.send('install-folder-ROC')
     } else if (Mode_Switch_State && !BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-folder-noc-ROC')
+        ElectronService.ipcRenderer.send('install-folder-noc-ROC')
     } else if (!Mode_Switch_State && BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-map-ROC')
+        ElectronService.ipcRenderer.send('install-map-ROC')
     } else if (!Mode_Switch_State && !BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-map-noc-ROC')
+        ElectronService.ipcRenderer.send('install-map-noc-ROC')
     }
 })
 
 
 image_Container_TFT.addEventListener('click', () => {
     if (Mode_Switch_State && BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-folder-TFT')
+        ipcRenderer.send('install-folder-TFT')
     } else if (Mode_Switch_State && !BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-folder-noc-TFT')
+        ipcRenderer.send('install-folder-noc-TFT')
     } else if (!Mode_Switch_State && BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-map-TFT')
+        ipcRenderer.send('install-map-TFT')
     } else if (!Mode_Switch_State && !BJ_Switch_State) {
-        this.electronService.ipcRenderer.send('install-map-noc-TFT')
+        ipcRenderer.send('install-map-noc-TFT')
     }
 })
 
