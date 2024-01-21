ab485f66fd9070513949b56eb749d6f7a43f5cee
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-21 14:18:37 +0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,47 +1,39 @@
-import { ipcRenderer } from 'electron';
+const { ipcRenderer } = ('electron')
 
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const Button_ROC = document.getElementById('imageContainerROC');
-    const Button_TFT = document.getElementById('imageContainerTFT');
-    const Button_REF = document.getElementById('imageContainerREF');
-    if (Button_ROC) {
-        button.addEventListener('click', () => {
-            if (Mode_Switch_State && BJ_Switch_State) {
-                ipcRenderer.send('install-folder-ROC');
-            } else if (Mode_Switch_State && !BJ_Switch_State) {
-                ipcRenderer.send('install-folder-noc-ROC');
-            } else if (!Mode_Switch_State && BJ_Switch_State) {
-                ipcRenderer.send('install-map-ROC');
-            } else if (!Mode_Switch_State && !BJ_Switch_State) {
-                ipcRenderer.send('install-map-noc-ROC');
-            }
-        }
-    )}
-    if (Button_TFT) {
-        button.addEventListener('click', () => {
-            if (Mode_Switch_State && BJ_Switch_State) {
-                ipcRenderer.send('install-folder-TFT');
-            } else if (Mode_Switch_State && !BJ_Switch_State) {
-                ipcRenderer.send('install-folder-noc-TFT');
-            } else if (!Mode_Switch_State && BJ_Switch_State) {
-                ipcRenderer.send('install-map-TFT');
-            } else if (!Mode_Switch_State && !BJ_Switch_State) {
-                ipcRenderer.send('install-map-noc-TFT');
-            }
-        }
-    )}
-    if (Button_REF) {
-        button.addEventListener('click', () => {
-            if (Mode_Switch_State && BJ_Switch_State) {
-                ipcRenderer.send('install-folder');
-            } else if (Mode_Switch_State && !BJ_Switch_State) {
-                ipcRenderer.send('install-folder-noc');
-            } else if (!Mode_Switch_State && BJ_Switch_State) {
-                ipcRenderer.send('install-map');
-            } else if (!Mode_Switch_State && !BJ_Switch_State) {
-                ipcRenderer.send('install-map-noc');
-            }
-        }
-        )}
-});
+image_Container_ROC.addEventListener('click', () => {
+    if (Mode_Switch_State && BJ_Switch_State) {
+        ipcRenderer.send('install-folder-ROC')
+    } else if (Mode_Switch_State && !BJ_Switch_State) {
+        ipcRenderer.send('install-folder-noc-ROC')
+    } else if (!Mode_Switch_State && BJ_Switch_State) {
+        ipcRenderer.send('install-map-ROC')
+    } else if (!Mode_Switch_State && !BJ_Switch_State) {
+        ipcRenderer.send('install-map-noc-ROC')
+    }
+})
+
+
+image_Container_TFT.addEventListener('click', () => {
+    if (Mode_Switch_State && BJ_Switch_State) {
+        ipcRenderer.send('install-folder-TFT')
+    } else if (Mode_Switch_State && !BJ_Switch_State) {
+        ipcRenderer.send('install-folder-noc-TFT')
+    } else if (!Mode_Switch_State && BJ_Switch_State) {
+        ipcRenderer.send('install-map-TFT')
+    } else if (!Mode_Switch_State && !BJ_Switch_State) {
+        ipcRenderer.send('install-map-noc-TFT')
+    }
+})
+
+image_Container_REF.addEventListener('click', () => {
+    if (Mode_Switch_State && BJ_Switch_State) {
+        ipcRenderer.send('install-folder')
+    } else if (Mode_Switch_State && !BJ_Switch_State) {
+        ipcRenderer.send('install-folder-noc')
+    } else if (!Mode_Switch_State && BJ_Switch_State) {
+        ipcRenderer.send('install-map')
+    } else if (!Mode_Switch_State && !BJ_Switch_State) {
+        ipcRenderer.send('install-map-noc')
+    }
+})
