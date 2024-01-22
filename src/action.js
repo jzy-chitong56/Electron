24a735f6c7c207470a195eb4f2f6e56fc47d6986
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-22 18:32:56 +0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,15 +1,35 @@
 const { ipcRenderer } = require('src/app/core/services/electron/electron.service');
-const modeState = Mode_Switch_State ? '-folder' : '-map';
-const bjState = BJ_Switch_State ? '' : '-noc';
+
+function sendInstallationMessage(ver) {
+  let modeState = Mode_Switch_State ? '-folder' : '-map';
+  let bjState = BJ_Switch_State ? '' : '-noc';
+  const message = `install${modeState}${bjState}${ver}`;
+}
 
 image_Container_ROC.addEventListener('click', () => {
-    ElectronService.ipcRenderer.send('install${modeState}${bjState}-ROC');
+    sendInstallationMessage('-ROC');
 });
-
 image_Container_TFT.addEventListener('click', () => {
-    ipcRenderer.send('install${modeState}${bjState}-TFT');
+    sendInstallationMessage('-TFT');
+});
+image_Container_REF.addEventListener('click', () => {
+    sendInstallationMessage('');
 });
 
-image_Container_REF.addEventListener('click', () => {
-    this.electronService.ipcRenderer.send('install${modeState}${bjState}');
-});
+// image_Container_ROC.addEventListener('click', () => {
+//     let modeState = Mode_Switch_State ? '-folder' : '-map';
+//     let bjState = BJ_Switch_State ? '' : '-noc';
+//     ElectronService.ipcRenderer.send('install${modeState}${bjState}-ROC');
+// });
+
+// image_Container_TFT.addEventListener('click', () => {
+//     let modeState = Mode_Switch_State ? '-folder' : '-map';
+//     let bjState = BJ_Switch_State ? '' : '-noc';
+//     ipcRenderer.send('install${modeState}${bjState}-TFT');
+// });
+
+// image_Container_REF.addEventListener('click', () => {
+//     let modeState = Mode_Switch_State ? '-folder' : '-map';
+//     let bjState = BJ_Switch_State ? '' : '-noc';
+//     this.electronService.ipcRenderer.send('install${modeState}${bjState}');
+// });
