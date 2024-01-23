01e1624af17ac8d30bd13742ebcf44d21744c549
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-23 20:08:19 +0800

    Update action.js

--- src/action.js
+++ src/action.js
@@ -1,10 +1,10 @@
-//const { ipcRenderer } = require('electron');
-window.electron = require('electron');
+const { ipcRenderer } = require('electron');
+//window.electron = require('electron');
 
 function installAction(ver, Mode_Switch_State, BJ_Switch_State) {
   let modeState = Mode_Switch_State ? '-folder' : '-map';
   let bjState = BJ_Switch_State ? '' : '-noc';
-  window.electron.ipcRenderer.send('install${modeState}${bjState}ver');
+  ipcRenderer.send('install${modeState}${bjState}ver');
   //     this.electronService.ipcRenderer.send('install${modeState}${bjState}ver');
   //     ElectronService.ipcRenderer.send('install${modeState}${bjState}ver');
 }
