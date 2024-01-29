c28bdc21372807fa3bc87d1761b98f05a3505122
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-29 17:31:28 +0800

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { ElectronService } from 'src/app/core/services/electron/electron.service';
-
+import { ElectronService } from './src/app/core/services/electron/electron.service';
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-home',
@@ -9,17 +11,17 @@ import { ElectronService } from 'src/app/core/services/electron/electron.service
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('ImagesROCEnable', { static: false }) Images_ROC_Enable: ElementRef;
-  @ViewChild('ImagesROCDisable', { static: false }) Images_ROC_Disable: ElementRef;
-  @ViewChild('imageROC', { static: false }) image_Container_ROC: ElementRef;
-  @ViewChild('ImagesTFTEnable', { static: false }) Images_TFT_Enable: ElementRef;
-  @ViewChild('ImagesTFTDisable', { static: false }) Images_TFT_Disable: ElementRef;
-  @ViewChild('imageTFT', { static: false }) image_Container_TFT: ElementRef;
-  @ViewChild('ImagesREFEnable', { static: false }) Images_REF_Enable: ElementRef;
-  @ViewChild('ImagesREFDisable', { static: false }) Images_REF_Disable: ElementRef;
-  @ViewChild('imageREF', { static: false }) image_Container_REF: ElementRef;
-  @ViewChild('Mode_Switch_State', { static: false }) ModeSwitch: ElementRef;
-  @ViewChild('BJ_Switch_State', { static: false }) BJSwitch: ElementRef;
+  @ViewChild('ImagesROCEnable', { static: true }) Images_ROC_Enable: ElementRef;
+  @ViewChild('ImagesROCDisable', { static: true }) Images_ROC_Disable: ElementRef;
+  @ViewChild('imageROC', { static: true }) image_Container_ROC: ElementRef;
+  @ViewChild('ImagesTFTEnable', { static: true }) Images_TFT_Enable: ElementRef;
+  @ViewChild('ImagesTFTDisable', { static: true }) Images_TFT_Disable: ElementRef;
+  @ViewChild('imageTFT', { static: true }) image_Container_TFT: ElementRef;
+  @ViewChild('ImagesREFEnable', { static: true }) Images_REF_Enable: ElementRef;
+  @ViewChild('ImagesREFDisable', { static: true }) Images_REF_Disable: ElementRef;
+  @ViewChild('imageREF', { static: true }) image_Container_REF: ElementRef;
+  @ViewChild('Mode_Switch_State', { static: true }) ModeSwitch: ElementRef;
+  @ViewChild('BJ_Switch_State', { static: true }) BJSwitch: ElementRef;
   ROCShown: boolean = false; 
   TFTShown: boolean = false; 
   REFShown: boolean = false; 
@@ -94,7 +96,7 @@ export class HomeComponent implements OnInit {
       default:
         console.log('Mouse out an element');
         break;
-  }
+    }
   }
 
   // 点击事件处理函数
@@ -156,14 +158,8 @@ export class HomeComponent implements OnInit {
       default:
         console.log('Mouse click an element');
         break;
-  }
+    }
   }
 
 
 }
-
-
-
-
-
-
