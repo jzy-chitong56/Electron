e36e635bb943b39394ab78cc6e6aa9d09d08820a
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-31 12:30:02 +0800

    Update home.service.ts

--- src/app/core/services/home/home.service.ts
+++ src/app/core/services/home/home.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@angular/core';
-import { ElectronService } from '../electron/electron.service';
-
 @Injectable({
   providedIn: 'root'
 })
@@ -10,8 +8,6 @@ export class HomeService {
   
 
   public changeEnabledHomeState(state: boolean) {
-    if(this.electronService.isElectron) { 
       this.isInteractive = state;
-    }
   }
 }
