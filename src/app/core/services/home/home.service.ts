53ed728f695efe74177a3a846b6fee2c7088070c
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-31 15:12:21 +0800

    Update home.service.ts

--- src/app/core/services/home/home.service.ts
+++ src/app/core/services/home/home.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-@Injectable({
-  providedIn: 'root'
-})
+// import { Injectable } from '@angular/core';
+// @Injectable({
+//   providedIn: 'root'
+// })
 
 export class HomeService {
   private _disableHomeState = false;
