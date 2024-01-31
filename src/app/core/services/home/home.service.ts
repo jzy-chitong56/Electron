09981e95534cc4a26182946ab3beb590e8aa607f
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-01-31 10:41:11 +0800

    Update home.service.ts

--- src/app/core/services/home/home.service.ts
+++ src/app/core/services/home/home.service.ts
@@ -8,8 +8,6 @@ import { ElectronService } from '../electron/electron.service';
 export class HomeService {
   constructor() { }
   
-  // 安装判断，设置交互
-  @HostBinding('disabled') isInteractive = false;
 
   public changeEnabledHomeState(state: boolean) {
     if(this.electronService.isElectron) { 
