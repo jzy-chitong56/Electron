0f6e1ff8d7c1d7cf09300afb88277f457e30d8df
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-02 13:50:24 +0800

    Update menu.service.ts

--- src/app/core/services/menu/menu.service.ts
+++ src/app/core/services/menu/menu.service.ts
@@ -7,81 +7,81 @@ import { ElectronService } from '../electron/electron.service';
 })
 export class MenuService {
   public template: any = [
-    {
-      label: '安装',
-      submenu: [
-        {
-          label: '安装重制版冰封王座到指定文件夹',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-folder');
-          }
-        },
-        {
-          label: '安装重制版冰封王座到指定文件夹(不装控制台)',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-folder-noc');
-          }
-        },
-        {
-          label: '安装重制版冰封王座到指定地图',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-map');
-          }
-        },
-        {
-          label: '安装重制版冰封王座到指定地图(不装控制台)',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-map-noc');
-          }
-        },
-        {
-          label: '安装经典版冰封王座到指定文件夹',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-folder-TFT');
-          }
-        },
-        {
-          label: '安装经典版冰封王座到指定文件夹(不装控制台)',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-folder-noc-TFT');
-          }
-        },
-        {
-          label: '安装经典版冰封王座到指定地图',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-map-TFT');
-          }
-        },
-        {
-          label: '安装经典版冰封王座到指定地图(不装控制台)',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-map-noc-TFT');
-          }
-        },
-                {
-          label: '安装经典版混乱之治到指定文件夹',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-folder-ROC');
-          }
-        },
-        {
-          label: '安装经典版混乱之治到指定文件夹(不装控制台)',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-folder-noc-ROC');
-          }
-        },
-        {
-          label: '安装经典版混乱之治到指定地图',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-map-ROC');
-          }
-        },
-        {
-          label: '安装经典版混乱之治到指定地图(不装控制台)',
-          click: () => {
-            this.electronService.ipcRenderer.send('install-map-noc-ROC');
-          }
-        }
+    //{
+      // label: '安装',
+      // submenu: [
+      //   {
+      //     label: '安装重制版冰封王座到指定文件夹',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-folder');
+      //     }
+      //   },
+      //   {
+      //     label: '安装重制版冰封王座到指定文件夹(不装控制台)',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-folder-noc');
+      //     }
+      //   },
+      //   {
+      //     label: '安装重制版冰封王座到指定地图',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-map');
+      //     }
+      //   },
+      //   {
+      //     label: '安装重制版冰封王座到指定地图(不装控制台)',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-map-noc');
+      //     }
+      //   },
+      //   {
+      //     label: '安装经典版冰封王座到指定文件夹',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-folder-TFT');
+      //     }
+      //   },
+      //   {
+      //     label: '安装经典版冰封王座到指定文件夹(不装控制台)',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-folder-noc-TFT');
+      //     }
+      //   },
+      //   {
+      //     label: '安装经典版冰封王座到指定地图',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-map-TFT');
+      //     }
+      //   },
+      //   {
+      //     label: '安装经典版冰封王座到指定地图(不装控制台)',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-map-noc-TFT');
+      //     }
+      //   },
+      //           {
+      //     label: '安装经典版混乱之治到指定文件夹',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-folder-ROC');
+      //     }
+      //   },
+      //   {
+      //     label: '安装经典版混乱之治到指定文件夹(不装控制台)',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-folder-noc-ROC');
+      //     }
+      //   },
+      //   {
+      //     label: '安装经典版混乱之治到指定地图',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-map-ROC');
+      //     }
+      //   },
+      //   {
+      //     label: '安装经典版混乱之治到指定地图(不装控制台)',
+      //     click: () => {
+      //       this.electronService.ipcRenderer.send('install-map-noc-ROC');
+      //     }
+      //  }
         // TODO: recreate MakeTFT script
         // FIXME: convert MakeTFTBase.bat to JS script
         // FIXME: convert MakeTFT.bat to JS script
@@ -96,21 +96,33 @@ export class MenuService {
         // { label: 'Compile, Optimize' },
         // TODO: recreate MakeVAITFT.bat script
         // { label: 'Compile AMAI vs Default AI' },
-      ]
-    },
+      //]
+    //},
+    // {
+    //   label: '查看',
+    //   submenu: [
+    //     { role: 'reload' },
+    //     { role: 'forceReload' },
+    //     { role: 'toggleDevTools' },
+    //     { type: 'separator' },
+    //     { role: 'resetZoom' },
+    //     { role: 'zoomIn' },
+    //     { role: 'zoomOut' },
+    //     { type: 'separator' },
+    //     { role: 'togglefullscreen' }
+    //   ]
+    // },
     {
-      label: '查看',
-      submenu: [
-        { role: 'reload' },
-        { role: 'forceReload' },
-        { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
-        { type: 'separator' },
-        { role: 'togglefullscreen' }
-      ]
+      {  
+        label: '切换开发者工具',  
+        //label: {{ 'PAGES.MUSE.DEV_TOOL' | translate }}',  
+        role: 'toggleDevTools'  
+      },  
+      {  
+        label: '全屏',  
+        //label: {{ 'PAGES.MUSE.FULLSCREEN' | translate }}',  
+        role: 'togglefullscreen'  
+      },  
     },
   ]
 
