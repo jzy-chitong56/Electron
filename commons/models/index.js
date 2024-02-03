fb1099599b63c50a5f25e2d832edf81f92dfe979
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-03 19:30:14 +0800

    Update index.js

--- commons/models/index.js
+++ commons/models/index.js
@@ -3,9 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.InstallModel = void 0;
 class InstallModel {
 }
-exports.InstallModel = InstallModel;
-exports.TransModel = void 0;
-class TransModel {
-}
-exports.TransModel = TransModel;
 //# sourceMappingURL=index.js.map
