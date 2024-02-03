1ca80f8d1168d9bdfc970883df6af672b777633a
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-03 19:23:19 +0800

    Update index.js

--- commons/models/index.js
+++ commons/models/index.js
@@ -4,4 +4,8 @@ exports.InstallModel = void 0;
 class InstallModel {
 }
 exports.InstallModel = InstallModel;
+exports.TransModel = void 0;
+class TransModel {
+}
+exports.TransModel = TransModel;
 //# sourceMappingURL=index.js.map
