c7fe66cc8554987189505e41bba4a36d7164f4c9
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-03 12:16:38 +0800

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -38,28 +38,27 @@ export class HomeComponent implements OnInit {
     });
   }
 
-
   // 鼠标移入事件处理函数
   @HostListener('mouseenter', ['$event', '$event.target.dataset.action'])
   onMouseEnter(event: MouseEvent, action: string) {
     if (this.isInteractive) {
-    switch (action) {
-     case 'Roc':
-        if (!this.ROCInstall) {
-          this.Images_ROC_Shown = true;
-        }
-        break;
-     case 'Tft':
-        if (!this.TFTInstall) {
-          this.Images_TFT_Shown = true;
-        }
-        break;
-     case 'Ref':
-        if (!this.REFInstall) {
-          this.Images_REF_Shown = true;
-        }
-        break;
-    }
+      switch (action) {
+       case 'Roc':
+          if (!this.ROCInstall) {
+            this.Images_ROC_Shown = true;
+          }
+          break;
+       case 'Tft':
+          if (!this.TFTInstall) {
+            this.Images_TFT_Shown = true;
+          }
+          break;
+       case 'Ref':
+          if (!this.REFInstall) {
+            this.Images_REF_Shown = true;
+          }
+          break;
+      }
     };
   }
 
@@ -67,23 +66,23 @@ export class HomeComponent implements OnInit {
   @HostListener('mouseout', ['$event', '$event.target.dataset.action'])
   onMouseLeave(event: MouseEvent, action: string) {
     if (this.isInteractive) { 
-    switch (action) {
-      case 'Roc':
-        if (!this.ROCInstall) {
-          this.Images_ROC_Shown = false;
-        }
-        break;
-      case 'Tft':
-        if (!this.TFTInstall) {
-          this.Images_TFT_Shown = false;
-        }
-        break;
-      case 'Ref':
-        if (!this.REFInstall) {
-          this.Images_REF_Shown = false;
-        }
-        break;
-    }
+      switch (action) {
+        case 'Roc':
+          if (!this.ROCInstall) {
+            this.Images_ROC_Shown = false;
+          }
+          break;
+        case 'Tft':
+          if (!this.TFTInstall) {
+            this.Images_TFT_Shown = false;
+          }
+          break;
+        case 'Ref':
+          if (!this.REFInstall) {
+            this.Images_REF_Shown = false;
+          }
+          break;
+      }
     };
   }
 
@@ -91,71 +90,63 @@ export class HomeComponent implements OnInit {
   @HostListener('click', ['$event', '$event.target.dataset.action'])
   onClick(event: MouseEvent, action: string) {
     if (this.isInteractive) { 
-    switch (action) {
-      case 'Roc':
-        if (!this.ROCInstall) {
-          let modeState = this.Mode_State ? '-folder' : '-map';
-          let bjState = this.BJ_State ? '' : '-noc';
-          const message = `install${modeState}${bjState}-ROC`;
-          this.Images_ROC_Shown = true;
-          this.Images_TFT_Shown = false;
-          this.Images_REF_Shown = false;
-          this.TFTInstall = false;
-          this.REFInstall = false;
-          this.electronService.ipcRenderer.send(message);
-        } else {
-          this.Images_ROC_Shown = false;
-        }
-        this.ROCInstall = !this.ROCInstall;
-        break;
-      case 'Tft':
-        if (!this.TFTInstall) {
-          let modeState = this.Mode_State ? '-folder' : '-map';
-          let bjState = this.BJ_State ? '' : '-noc';
-          const message = `install${modeState}${bjState}-TFT`;
-          this.Images_ROC_Shown = false;
-          this.Images_TFT_Shown = true;
-          this.Images_REF_Shown = false;
-          this.ROCInstall = false;
-          this.REFInstall = false;
-          this.electronService.ipcRenderer.send(message);
-        } else {
-          this.Images_TFT_Shown = false;
-        }
-        this.TFTInstall = !this.TFTInstall;
-        break;
-      case 'Ref':
-        if (!this.REFInstall) {
-          let modeState = this.Mode_State ? '-folder' : '-map';
-          let bjState = this.BJ_State ? '' : '-noc';
-          const message = `install${modeState}${bjState}`;
-          this.Images_ROC_Shown = false;
-          this.Images_TFT_Shown = false;
-          this.Images_REF_Shown = true;
-          this.ROCInstall = false;
-          this.TFTInstall = false;
-          this.electronService.ipcRenderer.send(message);
-        } else {
-          this.Images_REF_Shown = false;
-        }
-        this.REFInstall = !this.REFInstall;
-        break;
-      case 'Mode':
-        this.Mode_State = !this.Mode_State;
-        break;
-      case 'BJ':
-        this.BJ_State = !this.BJ_State;
-        break;
-    }
+      switch (action) {
+        case 'Roc':
+          if (!this.ROCInstall) {
+            let modeState = this.Mode_State ? '-folder' : '-map';
+            let bjState = this.BJ_State ? '' : '-noc';
+            const message = `install${modeState}${bjState}-ROC`;
+            this.Images_ROC_Shown = true;
+            this.Images_TFT_Shown = false;
+            this.Images_REF_Shown = false;
+            this.TFTInstall = false;
+            this.REFInstall = false;
+            this.ROCInstall = !this.ROCInstall;
+            this.electronService.ipcRenderer.send(message);
+          }
+          break;
+        case 'Tft':
+          if (!this.TFTInstall) {
+            let modeState = this.Mode_State ? '-folder' : '-map';
+            let bjState = this.BJ_State ? '' : '-noc';
+            const message = `install${modeState}${bjState}-TFT`;
+            this.Images_ROC_Shown = false;
+            this.Images_TFT_Shown = true;
+            this.Images_REF_Shown = false;
+            this.ROCInstall = false;
+            this.REFInstall = false;
+            this.TFTInstall = !this.TFTInstall;
+            this.electronService.ipcRenderer.send(message);
+          }
+          break;
+        case 'Ref':
+          if (!this.REFInstall) {
+            let modeState = this.Mode_State ? '-folder' : '-map';
+            let bjState = this.BJ_State ? '' : '-noc';
+            const message = `install${modeState}${bjState}`;
+            this.Images_ROC_Shown = false;
+            this.Images_TFT_Shown = false;
+            this.Images_REF_Shown = true;
+            this.ROCInstall = false;
+            this.TFTInstall = false;
+            this.REFInstall = !this.REFInstall;
+            this.electronService.ipcRenderer.send(message);
+          }
+          break;
+        case 'Mode':
+          this.Mode_State = !this.Mode_State;
+          break;
+        case 'BJ':
+          this.BJ_State = !this.BJ_State;
+          break;
+      }
     };
   }
-
 
   constructor(
     private router: Router,
     private electronService: ElectronService,
     private homeService: HomeService,
   ) { }
-
 
 }
