0726e4d724932756c2af726da785c049ef1aca81
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-04 18:06:38 +0800

    Update home.service.ts

--- src/app/core/services/home/home.service.ts
+++ src/app/core/services/home/home.service.ts
@@ -18,22 +18,3 @@ export class HomeService {
     return this.enableHomeInteractive;
   }
 }
-
-// import { Injectable } from '@angular/core';  
-
-// @Injectable({  
-//   providedIn: 'root'  
-// })  
-// export class HomeService {  
-//   private enableHomeInteractive = true;  
-  
-//   constructor() {}  
-  
-//   changeEnabledHomeState(state: boolean) {  
-//     this.enableHomeInteractive = state;  
-//   }  
-  
-//   get enableHomeState(): boolean {  
-//     return this.enableHomeInteractive;  
-//   }  
-// }
