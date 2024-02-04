c1997689aca908b67659914615eacf229c5f2a06
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-04 10:17:38 +0800

    Update menu.service.ts

--- src/app/core/services/menu/menu.service.ts
+++ src/app/core/services/menu/menu.service.ts
@@ -38,7 +38,7 @@ export class MenuService {
       //   this.template[2].label = res;
       //   console.log("Menu 2", res);
       // }); 
-      this.template[2].label =this.translate.instant('PAGES.MUSE.DEV_TOOL')
+      this.template[2].label = this.translate.instant('PAGES.MUSE.DEV_TOOL')
       console.log("Menu 2", res);
       const { Menu } = this.electronService;
       const menu = Menu.buildFromTemplate(this.template);
