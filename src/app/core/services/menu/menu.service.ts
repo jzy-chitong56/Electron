989029b854a8344af958c57bf8378ec993e90776
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-02-04 12:30:32 +0800

    Update menu.service.ts

--- src/app/core/services/menu/menu.service.ts
+++ src/app/core/services/menu/menu.service.ts
@@ -37,6 +37,8 @@ export class MenuService {
       }).subscribe((res) => {
         this.template[1].label = res.fullscreen;
         this.template[2].label = res.devTool;
+        console.log('menu1', res.fullscreen);
+        console.log('menu2', res.devTool);
         const { Menu } = this.electronService;
         const menu = Menu.buildFromTemplate(this.template);
         Menu.setApplicationMenu(menu);
