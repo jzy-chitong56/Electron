7bd35759fa577b4006d358abfd67b1e23db1cf78
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2024-09-25 21:07:03 +0800

    Update install.js

--- AMAI-release/install.js
+++ AMAI-release/install.js
@@ -31,9 +31,12 @@ const getAllFiles = (dirPath, arrayOfFiles) => {
 const installOnDirectory = async () => {
   const args = process.argv.slice(2);
   const installCommander = (args[1] == 'true');
+  const installCommanderMode1 = (args[1] == '1');
+  const installCommanderMode2 = (args[1] == '2');
+  const installCommanderMode3 = (args[1] == '0');
   const response = args[0];
   const ver = args[2];
-  const icon = (args[2] == "REFORGED")
+  const icon = (args[2] == "REFORGED");
   process.send(`#### 正在安装 AMAI ${ver} ####`);
 
   // TODO: change to receive array of maps
@@ -53,8 +56,18 @@ const installOnDirectory = async () => {
     process.send(`错误: 找不到 ${process.cwd()}\\MPQEditor.exe, 请百度下载 MPQEditor.exe 并放入以下目录: ${process.cwd()}`)
     return
   }
-  if (installCommander && !fs.existsSync(`Scripts\\Blizzard_${ver}.j`)) {
-    process.send(`错误: 找不到 ${process.cwd()}\\Scripts\\blizzard_${ver}.j, 请重新编译后将文件放入以下目录: ${process.cwd()}\\Scripts\\${ver}\\`)
+  // if (installCommander && !fs.existsSync(`Scripts\\Blizzard_${ver}.j`)) {
+  //   process.send(`错误: 找不到 ${process.cwd()}\\Scripts\\blizzard_${ver}.j, 请重新编译后将文件放入以下目录: ${process.cwd()}\\Scripts\\${ver}\\`)
+  //   return
+  // }
+
+  if (installCommanderMode1 && !fs.existsSync(`Scripts\\${ver}\\Blizzard.j`)) {
+    process.send(`错误: 找不到 ${process.cwd()}\\Scripts\\${ver}\\blizzard.j`)
+    return
+  }
+
+  if (installCommanderMode2 && !fs.existsSync(`Scripts\\${ver}\\Blizzard_VSAI.j`)) {
+    process.send(`错误: 找不到 ${process.cwd()}\\Scripts\\${ver}\\blizzard_VSAI.j`)
     return
   }
 
@@ -89,7 +102,7 @@ const installOnDirectory = async () => {
         );
 
         /** uncomment to debbug */
-       // console.log('mpqEditor', mpqEditor.error);
+        // console.log('mpqEditor', mpqEditor.error);
 
         // spawnSync(`echo`, [`running execuMPQEditor ${file}`]);
         if (mpqEditor.status == 5) {
@@ -100,7 +113,7 @@ const installOnDirectory = async () => {
           process.send(mpqEditor.error.message)
             : process.send(`设置最大写入文件数到 ${file}`);
 
-        const f1AddToMPQ =  spawnSync(
+        const f1AddToMPQ = spawnSync(
           `MPQEditor.exe`,
           [
             'a',
@@ -112,7 +125,7 @@ const installOnDirectory = async () => {
         );
 
         /** uncomment to debbug */
-       // console.log('f1AddToMPQ', f1AddToMPQ.error);
+        // console.log('f1AddToMPQ', f1AddToMPQ.error);
 
         // spawnSync(`echo`, [`running AddToMPQ 1 ${file}`]);
         // process.send(`running AddToMPQ 1 ${file}`);
@@ -124,39 +137,65 @@ const installOnDirectory = async () => {
           process.send(f1AddToMPQ.error.message)
             : process.send(`写入 AI 脚本到 ${file}`);
 
-        if (installCommander) {
+        if (installCommanderMode1 || installCommanderMode2) {
 
-          const f2AddToMPQ =  spawnSync(
-            `MPQEditor.exe`,
-            [
-              'a',
-              file,
-              `Scripts\\Blizzard_${ver}.j`,
-              `Scripts\\Blizzard.j`,
-            ],
-            { encoding : `utf8` }
-          );
+          if (installCommanderMode1 ) {
+            const f2AddToMPQ = spawnSync(
+              `MPQEditor.exe`,
+              [
+                'a',
+                file,
+                `Scripts\\${ver}\\Blizzard.j`,
+                `Scripts\\Blizzard.j`
+              ],
+              { encoding : `utf8` }
+            );
 
-          /** uncomment to debbug */
-          // console.log('f2AddToMPQ', f2AddToMPQ.error);
+            /** uncomment to debbug */
+            // console.log('f2AddToMPQ', f2AddToMPQ.error);
 
-          // spawnSync(`echo`, [`running AddToMPQ 2 ${file}`]);
-          if (f2AddToMPQ.status == 5) {
-            process.send(`警告: ${file} 写入控制台失败, 您可能不具备权限，或被 UAC 阻止。请确保地图文件不在受 UAC 保护的目录`)
-            continue;
+            // spawnSync(`echo`, [`running AddToMPQ 2 ${file}`]);
+            if (f2AddToMPQ.status == 5) {
+              process.send(`警告: ${file} 写入控制台失败, 您可能不具备权限，或被 UAC 阻止。请确保地图文件不在受 UAC 保护的目录`)
+              continue;
+            }
+            f2AddToMPQ.error ?
+              process.send(f2AddToMPQ.error.message)
+                : process.send(`写入控制台到 ${file}`);
+          } else if (installCommanderMode2) {
+
+            const f2AddToMPQ = spawnSync(
+              `MPQEditor.exe`,
+              [
+                'a',
+                file,
+                `Scripts\\${ver}\\Blizzard_VSAI.j`,
+                `Scripts\\Blizzard.j`
+              ],
+              { encoding : `utf8` }
+            );
+
+            /** uncomment to debbug */
+            // console.log('f2AddToMPQ', f2AddToMPQ.error);
+
+            // spawnSync(`echo`, [`running AddToMPQ 2 ${file}`]);
+            if (f2AddToMPQ.status == 5) {
+              process.send(`警告: ${file} 写入 VS AI 控制台失败, 您可能不具备权限，或被 UAC 阻止。请确保地图文件不在受 UAC 保护的目录`)
+              continue;
+            }
+            f2AddToMPQ.error ?
+              process.send(f2AddToMPQ.error.message)
+                : process.send(`写入 VS AI 控制台到 ${file}`);
           }
-          f2AddToMPQ.error ?
-            process.send(f2AddToMPQ.error.message)
-              : process.send(`写入控制台到 ${file}`);
 
           if (icon) {
-            const f3AddToMPQ =  spawnSync(
+            const f3AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
                 'a',
                 file,
                 `Icons\\Reforged\\MiniMap\\`,
-                `UI\\MiniMap`,
+                `UI\\MiniMap`
               ],
             );
             if (f3AddToMPQ.status == 5) {
@@ -169,13 +208,13 @@ const installOnDirectory = async () => {
           }
 
           if (icon) {
-            const f4AddToMPQ =  spawnSync(
+            const f4AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
               'a',
               file,
               `Icons\\Reforged\\CommandButtons\\*.dds`,
-              `ReplaceableTextures\\CommandButtons`,
+              `ReplaceableTextures\\CommandButtons`
                 ],
             );
             if (f4AddToMPQ.status == 5) {
@@ -185,14 +224,14 @@ const installOnDirectory = async () => {
             f4AddToMPQ.error ?
             process.send(f4AddToMPQ.error.message)
               : process.send(`写入重置版物品图标到 ${file}`);
-          }else {
-            const f4AddToMPQ =  spawnSync(
+          } else {
+            const f4AddToMPQ = spawnSync(
                 `MPQEditor.exe`,
                 [
                 'a',
                 file,
                 `Icons\\Class\\CommandButtons\\*.blp`,
-                `ReplaceableTextures\\CommandButtons`,
+                `ReplaceableTextures\\CommandButtons`
                 ],
             );
             if (f4AddToMPQ.status == 5) {
@@ -205,13 +244,13 @@ const installOnDirectory = async () => {
           }
 
           if (icon) {
-            const f5AddToMPQ =  spawnSync(
+            const f5AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
                 'a',
                 file,
                 `Icons\\Reforged\\CommandButtonsDisabled\\*.dds`,
-                `ReplaceableTextures\\CommandButtonsDisabled`,
+                `ReplaceableTextures\\CommandButtonsDisabled`
               ],
             );
             if (f5AddToMPQ.status == 5) {
@@ -221,14 +260,14 @@ const installOnDirectory = async () => {
             f5AddToMPQ.error ?
             process.send(f5AddToMPQ.error.message)
               : process.send(`写入重置版物品阴影图标到 ${file}`);
-          }else {
-            const f5AddToMPQ =  spawnSync(
+          } else {
+            const f5AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
                 'a',
                 file,
                 `Icons\\Class\\CommandButtonsDisabled\\*.blp`,
-                `ReplaceableTextures\\CommandButtonsDisabled`,
+                `ReplaceableTextures\\CommandButtonsDisabled`
               ],
             );
             if (f5AddToMPQ.status == 5) {
@@ -241,13 +280,13 @@ const installOnDirectory = async () => {
           }
 
           if (icon) {
-            const f6AddToMPQ =  spawnSync(
+            const f6AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
                 'a',
                 file,
                 `Icons\\Reforged\\war3map.imp`,
-                `war3map.imp`,
+                `war3map.imp`
               ],
               { encoding : `utf8` }
             );
@@ -258,14 +297,14 @@ const installOnDirectory = async () => {
             f6AddToMPQ.error ?
               process.send(f6AddToMPQ.error.message)
                 : process.send(`写入重置版导入列表到 ${file}`);
-          }else {
-            const f6AddToMPQ =  spawnSync(
+          } else {
+            const f6AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
                 'a',
                 file,
                 `Icons\\Class\\war3map.imp`,
-                `war3map.imp`,
+                `war3map.imp`
               ],
               { encoding : `utf8` }
             );
@@ -279,13 +318,13 @@ const installOnDirectory = async () => {
           }
 
           if (icon) {
-            const f7AddToMPQ =  spawnSync(
+            const f7AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
                 'a',
                 file,
                 `Icons\\Reforged\\war3map.w3t`,
-                `war3map.w3t`,
+                `war3map.w3t`
               ],
               { encoding : `utf8` }
             );
@@ -296,14 +335,14 @@ const installOnDirectory = async () => {
             f7AddToMPQ.error ?
               process.send(f7AddToMPQ.error.message)
                 : process.send(`写入重置版物品图标索引到 ${file}`);
-          }else {
-            const f7AddToMPQ =  spawnSync(
+          } else {
+            const f7AddToMPQ = spawnSync(
               `MPQEditor.exe`,
               [
                 'a',
                 file,
                 `Icons\\Class\\war3map.w3t`,
-                `war3map.w3t`,
+                `war3map.w3t`
               ],
               { encoding : `utf8` }
             );
@@ -318,7 +357,7 @@ const installOnDirectory = async () => {
 
         }
 
-        const f8AddToMPQ =  spawnSync(
+        const f8AddToMPQ = spawnSync(
           `MPQEditor.exe`,
           [
             'f',
@@ -328,7 +367,7 @@ const installOnDirectory = async () => {
         );
 
         /** uncomment to debbug */
-       // console.log('f8AddToMPQ', f8AddToMPQ.error);
+        // console.log('f8AddToMPQ', f8AddToMPQ.error);
 
         // spawnSync(`echo`, [`running AddToMPQ 3 ${file}`]);
         if (f8AddToMPQ.status == 5) {
