06a0bdf5a6c89a74fd9ac5b57bf622b6494c2a0b
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2026-02-25 01:10:17 +0800

    Add files via upload

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,22 +12,119 @@ import { ElectronService } from '../core/services/electron/electron.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
   Images_ROC_Shown: boolean = false;
-  Images_TFT_Shown: boolean = false; 
-  Images_REF_Shown: boolean = false; 
-  ROCInstall: boolean = false; 
-  TFTInstall: boolean = false; 
-  REFInstall: boolean = false; 
+  Images_TFT_Shown: boolean = false;
+  Images_REF_Shown: boolean = false;
+  ROCInstall: boolean = false;
+  TFTInstall: boolean = false;
+  REFInstall: boolean = false;
   Mode_State: boolean = true;
-  BJ_State: boolean = true;
+  BJ_State: number = 1;
   isInteractive: boolean = true;
   modeState: string = '-folder';
   bjState: string = '';
   message: string = '';
+  optimize: boolean = true;
+  forcelang: boolean = false;
+  installEvent: string = 'install'
+  gamePaths = {
+    TFT: { 
+      PATH: null as string | null,
+      displayText: '--',
+    },
+    REFORGED: { 
+      PATH: null as string | null,
+      displayText: '--',
+    },
+    ROC: { 
+      PATH: null as string | null,
+      displayText: '--',
+    }
+  };
+
+  constructor(
+    private electronService: ElectronService, 
+  ) {}
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
+    this.loadDefaultPath();
+    if (this.electronService.isElectron) {
+      this.electronService.ipcRenderer.on('path-updated', (event, { pathver, path }) => {
+        console.log(`Path updated for ${pathver}:`, path);
+        this.gamePaths[pathver].PATH = path;
+        this.gamePaths[pathver].displayText = this.formatPath(path);
+      });
+    }
+  }
+  loadDefaultPath(): void {
+    if (this.electronService.isElectron) {
+      this.electronService.ipcRenderer.invoke('file-operations', {
+        operation: 'load-default-path'
+      }).then((settings: any) => {
+        console.log('Loaded settings:', settings);
+        if (settings) {
+          this.gamePaths.TFT.PATH = settings.TFT_PATH|| null;
+          this.gamePaths.REFORGED.PATH = settings.REFORGED_PATH|| null;
+          this.gamePaths.ROC.PATH = settings.ROC_PATH|| null;
+          this.gamePaths.TFT.displayText = this.gamePaths.TFT.PATH ? this.formatPath(this.gamePaths.TFT.PATH) : '--';
+          this.gamePaths.REFORGED.displayText = this.gamePaths.REFORGED.PATH ? this.formatPath(this.gamePaths.REFORGED.PATH) : '--';
+          this.gamePaths.ROC.displayText = this.gamePaths.ROC.PATH ? this.formatPath(this.gamePaths.ROC.PATH) : '--';
+          console.log('Path values after loading:');
+          console.log('REFORGED:', this.gamePaths.REFORGED.PATH, 'Display:', this.gamePaths.REFORGED.displayText);
+          console.log('TFT:', this.gamePaths.TFT.PATH, 'Display:', this.gamePaths.TFT.displayText);
+          console.log('ROC:', this.gamePaths.ROC.PATH, 'Display:', this.gamePaths.ROC.displayText);
+        }
+      }).catch(error => {
+        console.error('Error loading paths:', error);
+      });
+    }
+  }
+
+  private formatPath(path: string, maxLength = 20): string {
+    if (!path) return '--';
+    if (path.length <= maxLength) return path;
+    const parts = path.split(/[\\/]/);
+    if (parts.length <= 2) return path;
+    const firstPart = parts[0];
+    const lastPart = parts[parts.length - 1];
+    const middleLength = maxLength - (firstPart.length + lastPart.length + 5);
+    if (middleLength > 0) {
+      return `${firstPart}/.../${lastPart}`;
+    }
+    return `${firstPart}/...${lastPart}`;
+  }
+  async selectGameFolder(pathver: 'REFORGED' | 'TFT' | 'ROC'): Promise<void> {
+    try {
+      if (this.electronService.isElectron) {
+        console.log(`Selecting folder for ${pathver}`);
+        const result = await this.electronService.ipcRenderer.invoke('file-operations', {
+          operation: 'select-folder',
+          ver: pathver
+        });
+        if (result && result.length > 0) {
+          const selectedPath = result.filePaths[0];
+          this.gamePaths[pathver].PATH = selectedPath;
+          this.gamePaths[pathver].displayText = this.formatPath(selectedPath);
+          await this.electronService.ipcRenderer.invoke('file-operations', {
+            operation: 'save-default-path',
+            ver: pathver,
+            newpath: selectedPath
+          });
+          this.electronService.ipcRenderer.send('path-changed', {
+            type: pathver,
+            path: selectedPath
+          });
+          console.log(`${pathver} folder selected:`, selectedPath);
+          console.log(`${pathver} path set to:`, this.gamePaths[pathver].PATH);
+          console.log(`${pathver} display text:`, this.gamePaths[pathver].displayText);
+          console.log('Paths container visibility:',
+          this.gamePaths.TFT.PATH || this.gamePaths.REFORGED.PATH || this.gamePaths.ROC.PATH);
+        }
+      }
+    } catch (error) {
+      console.error(`${pathver} folder selection failed:`, error);
+    }
   }
 
   @HostListener('mouseenter', ['$event', '$event.target.dataset.action'])
@@ -53,7 +150,6 @@ export class HomeComponent implements OnInit {
     };
   }
 
-
   @HostListener('mouseout', ['$event', '$event.target.dataset.action'])
   onMouseLeave(event: MouseEvent, action: string) {
     if (this.isInteractive) { 
@@ -76,8 +172,6 @@ export class HomeComponent implements OnInit {
       }
     };
   }
-  
-
 
   @HostListener('click', ['$event', '$event.target.dataset.action'])
   onClick(event: MouseEvent, action: string) {
@@ -86,30 +180,39 @@ export class HomeComponent implements OnInit {
         case 'Roc':
           if (!this.ROCInstall) {
             this.message = `install${this.modeState}${this.bjState}-ROC`;
+            this.Images_ROC_Shown = true;
+            this.Images_TFT_Shown = false;
+            this.Images_REF_Shown = false;
             this.TFTInstall = false;
             this.REFInstall = false;
             this.ROCInstall = !this.ROCInstall;
-            this.electronService.ipcRenderer.send(this.message);
+            this.electronService.ipcRenderer.send(this.installEvent, 'ROC', this.Mode_State, this.BJ_State, this.optimize, this.forcelang);
             console.log('message',this.message);
           }
           break;
         case 'Tft':
           if (!this.TFTInstall) {
             this.message = `install${this.modeState}${this.bjState}-TFT`;
+            this.Images_ROC_Shown = false;
+            this.Images_TFT_Shown = true;
+            this.Images_REF_Shown = false;
             this.ROCInstall = false;
             this.REFInstall = false;
             this.TFTInstall = !this.TFTInstall;
-            this.electronService.ipcRenderer.send(this.message);
+            this.electronService.ipcRenderer.send(this.installEvent, 'TFT', this.Mode_State, this.BJ_State, this.optimize, this.forcelang);
             console.log('message',this.message);
           }
           break;
         case 'Ref':
           if (!this.REFInstall) {
             this.message = `install${this.modeState}${this.bjState}`;
+            this.Images_ROC_Shown = false;
+            this.Images_TFT_Shown = false;
+            this.Images_REF_Shown = true;
             this.ROCInstall = false;
             this.TFTInstall = false;
             this.REFInstall = !this.REFInstall;
-            this.electronService.ipcRenderer.send(this.message);
+            this.electronService.ipcRenderer.send(this.installEvent, 'REFORGED', this.Mode_State, this.BJ_State, this.optimize, this.forcelang);
             console.log('message',this.message);
           }
           break;
@@ -131,17 +234,34 @@ export class HomeComponent implements OnInit {
           this.modeState = this.Mode_State ? '-folder' : '-map';
           console.log('mode',this.modeState,this.Mode_State);
           break;
-        case 'BJSwitch':
-          this.BJ_State = !this.BJ_State;
-          this.bjState = this.BJ_State ? '' : '-noc';
-          console.log('BJ',this.bjState,this.BJ_State);
+        case 'BJoptionOn':
+          this.bjState = '';
+          this.BJ_State = 1;
+          console.log('BJ',this.bjState);
+          break;
+        case 'BJoptionVsAI':
+          this.bjState = '-vai';
+          this.BJ_State = 2;
+          console.log('BJ',this.bjState);
+          break;
+        case 'BJoptionOff':
+          this.bjState = '-noc';
+          this.BJ_State = 0;
+          console.log('BJ',this.bjState);
+          break;
+        case 'Optimise':
+          this.optimize = !this.optimize;
+          if (this.optimize) {
+            this.forcelang = false;
+          }
+          break;
+        case 'ForceLang':
+          this.forcelang = !this.forcelang;
+          if (this.forcelang) {
+            this.optimize = false;
+          }
           break;
       }
     };
   }
-
-  constructor(
-    private router: Router,
-    private electronService: ElectronService,
-  ) { }
 }
