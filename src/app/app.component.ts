12232c4011aefbbf4030130720d75a6156467f63
jzy-chitong56 <48715223+jzy-chitong56@users.noreply.github.com>
2026-03-01 16:27:26 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { AfterViewChecked, ChangeDetectorRef, Component, ElementRef, ViewChild } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { ElectronService, MenuService } from './core/services';
 import { TranslateService, TranslatePipe, TranslateDirective, _ as t_, LangChangeEvent } from "@codeandweb/ngx-translate";
 import { APP_CONFIG } from '../environments/environment';
@@ -19,12 +18,12 @@ export class AppComponent implements AfterViewChecked {
   public totalFiles = 0;
   public installingText = '';
 
-  @ViewChild('logareawrapper') private readonly logContainer: ElementRef; 
-    
-  ngAfterViewChecked() { this.scrollToBottom(); } 
-    
-  private scrollToBottom(): void { this.logContainer.nativeElement.scrollTop = this.logContainer.nativeElement.scrollHeight; } 
-  
+  @ViewChild('logareawrapper') private readonly logContainer: ElementRef;
+
+  ngAfterViewChecked() { this.scrollToBottom(); }
+
+  private scrollToBottom(): void { this.logContainer.nativeElement.scrollTop = this.logContainer.nativeElement.scrollHeight; }
+
   constructor(
     private readonly electronService: ElectronService,
     private readonly translate: TranslateService,
@@ -34,39 +33,44 @@ export class AppComponent implements AfterViewChecked {
     const lang = this.translate.getBrowserLang();
     this.translate.use(lang)
     console.log('APP_CONFIG', APP_CONFIG);
-    this.translate.get(t_('PAGES.APP.INSTALLING')).subscribe((res: string) => {
-      this.installingText = res;
-    });
 
     // Refresh app when language changes
     this.translate.onDefaultLangChange.subscribe((event: LangChangeEvent) => {
-      this.translate.get([t_('PAGES.HOME.TITLE'),t_('PAGES.ELECTRON.OPEN_MAP'), t_('PAGES.ELECTRON.OPEN_DIR'), t_('PAGES.ELECTRON.MAPFILE')]).subscribe((translations: { [key: string]: string } ) => {
+      this.translate.get([t_('PAGES.HOME.TITLE'), t_('PAGES.ELECTRON.OPEN_MAP'), t_('PAGES.ELECTRON.OPEN_DIR'), t_('PAGES.ELECTRON.MAPFILE')]).subscribe((translations: { [key: string]: string }) => {
         this.electronService.ipcRenderer.send('Trans', event.lang, translations);
       })
       this.cdr.detectChanges();
     });
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
-      this.translate.get([t_('PAGES.HOME.TITLE'),t_('PAGES.ELECTRON.OPEN_MAP'), t_('PAGES.ELECTRON.OPEN_DIR'), t_('PAGES.ELECTRON.MAPFILE')]).subscribe((translations: { [key: string]: string } ) => {
+      this.translate.get([t_('PAGES.HOME.TITLE'), t_('PAGES.ELECTRON.OPEN_MAP'), t_('PAGES.ELECTRON.OPEN_DIR'), t_('PAGES.ELECTRON.MAPFILE')]).subscribe((translations: { [key: string]: string }) => {
         this.electronService.ipcRenderer.send('Trans', event.lang, translations);
       })
       this.cdr.detectChanges();
     });
-    
 
     if (electronService.isElectron) {
       this.menuService.createMenu();
 
-      this.electronService.ipcRenderer.on('on-install-progress', (_, args: { total: number }) => {
-        this.totalFiles = args.total;
+      this.electronService.ipcRenderer.on('on-install-progress', (_, args: { current: number, total: number }) => {
+        // console.log('totalFiles-in:', args.total, 'currentFile-in:', args.current);
+        if (args.total > 0 && this.totalFiles < args.total) {
+          this.totalFiles = args.total;
+        }
+        if (this.currentFile < this.totalFiles) {
+          this.currentFile++;
+          this.title = '(' + this.currentFile + '/' + this.totalFiles + ') ' + this.installingText;
+        }
+        // console.log('totalFiles-out:', this.totalFiles, 'currentFile-out:', this.currentFile);
+        this.cdr.detectChanges();
       });
 
       // TODO: add 'push notification'/'notification'
       this.electronService.ipcRenderer.on('on-install-init', (_, args: InstallModel) => {
         console.log('args-install-init', args)
-        if (this.currentFile < this.totalFiles) {
-          this.currentFile++;
-        }
-        this.title = `${this.installingText} (${this.currentFile}/${this.totalFiles}) path: ${args.response}`;
+        this.translate.get(t_('PAGES.APP.INSTALLING'), {path: args.response}).subscribe((res: string) => {
+          this.title = '(0/X) ' + res;
+          this.installingText = res;
+        });
         this.active = true;
         this.couldClose = false;
         this.messages = [];
@@ -97,7 +101,7 @@ export class AppComponent implements AfterViewChecked {
       // TODO: add 'push notification'/'notification'
       this.electronService.ipcRenderer.on('on-install-exit', (_, args) => {
         this.translate.get(t_('PAGES.APP.INSTALL_DONE')).subscribe((res: string) => {
-          this.title = res;
+          this.title = '(' + this.currentFile + '/' + this.totalFiles + ')' + ' ' + res;
         });
         this.couldClose = true;
         this.totalFiles = 0;
@@ -109,10 +113,14 @@ export class AppComponent implements AfterViewChecked {
         this.cdr.detectChanges();
       });
 
-      // Handle standard messages
       this.electronService.ipcRenderer.on('on-install-message', (_, args) => {
         console.log('args-install-message', args);
         this.messages?.push(args);
+        this.cdr.detectChanges();
+      });
+
+      this.electronService.ipcRenderer.on('on-install-console', (_, args) => {
+        console.log('args-install-console', args);  // just console
         this.cdr.detectChanges();
       });
 
@@ -135,7 +143,7 @@ export class AppComponent implements AfterViewChecked {
   }
 
   public closeCmd() {
-    if (this.couldClose) { 
+    if (this.couldClose) {
       this.active = false;
     }
   }
